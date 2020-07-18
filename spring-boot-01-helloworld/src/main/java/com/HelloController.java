7553dfadc56f3b5d4510d50ef3e5deadc897390a
XUAN <1033027640@qq.com>
2020-07-18 16:46:42 +0800

    spring-boot-01-helloworld

--- spring-boot-01-helloworld/src/main/java/com/HelloController.java
+++ spring-boot-01-helloworld/src/main/java/com/HelloController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+/**
+ * 将主配置类（@SpringBootApplication标注的类）的所在包及下面所有子包里面的所有组件扫描到Spring容器
+ * 此类不在主配置类的所在包中，不可以扫描到 Spring 容器
+ */
 @Controller
 public class HelloController {
 
     @ResponseBody
-    @RequestMapping("/hello")
+    @RequestMapping("/NoScanIntoTheSpringContainer")
     public String hello(){
         return "Hello World!";
     }
--- spring-boot-01-helloworld/src/main/java/com/atguigu/controller/HelloController.java
+++ spring-boot-01-helloworld/src/main/java/com/atguigu/controller/HelloController.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 将主配置类（@SpringBootApplication标注的类）的所在包及下面所有子包里面的所有组件扫描到Spring容器
+ * 此类在主配置类的所在包中，可以扫描到 Spring 容器
+ */
 @Controller
 public class HelloController {
 
-//    @ResponseBody
-//    @RequestMapping("/hello")
-//    public String hello(){
-//        return "Hello World!";
-//    }
+    @ResponseBody
+    @RequestMapping("/ScanIntoTheSpringContainer")
+    public String hello(){
+        return "Hello World!";
+    }
 }
